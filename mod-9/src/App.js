0e89e3b112a2effb9c0c8e6d5afcbf8a65793dcf
FabianaRibas <fabianalv.ribas@gmail.com>
2023-09-04 19:17:37 -0300

    Handle submit anb reset button click events

--- mod-9/src/App.js
+++ mod-9/src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header/Header";
-import Form from "./components/UserInput/Form";
+import UserInput from "./components/UserInput/UserInput.js";
 import ResultsTable from "./components/ResultsTable/ResultsTable";
 
 function App() {
   return (
     <div>
       <Header />
-      <Form />
+      <UserInput />
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
